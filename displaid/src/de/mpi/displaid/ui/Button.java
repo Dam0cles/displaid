b4e999784c2833100657225dde92b4431710ba50
Benjamin Vahl <bvahl@net.t-labs.tu-berlin.de>
2012-01-23 17:28:48 +0100

    - Button has now setHover method
    Signed-off-by: Benjamin Vahl <bvahl@net.t-labs.tu-berlin.de>

--- displaid/src/de/mpi/displaid/ui/Button.java
+++ displaid/src/de/mpi/displaid/ui/Button.java
@@ -6,6 +6,11 @@ public class Button extends UserControl {
 
 	private String caption;
 	
+	/**
+	 * value between 0 and 1 representing progress until the Button is considered as pressed
+	 */
+	private float hover = 0.0f;
+	
 	private float red = 255,
 				  green = 128,
 				  blue = 0;
@@ -32,6 +37,30 @@ public class Button extends UserControl {
 		canvas.textAlign(canvas.CENTER, canvas.CENTER);
 		canvas.fill(255);
 		canvas.text(caption, x + width/2, y + height/2);
+		
+		if (hover > 0.0f) {
+			// draw hover progress as a growing rectangle
+			canvas.fill(canvas.color(red, green, blue, 255));
+			
+			float prog_start = 0,
+				  prog_dist = (width - 10) * hover,
+				  right_corner_radius = 0;
+				
+			if (hover >= 1.0f) {
+				prog_dist = width;
+				right_corner_radius = 10;
+			}
+			canvas.rect(x + prog_start, y, prog_dist, height, 10, right_corner_radius, 10, right_corner_radius);
+		}
 	}
 
+	public void setHover(float progress)
+	{
+		if (progress < 0)
+			progress = 0;
+		if (progress > 1.0f)
+			progress = 1;
+		
+		hover = progress;
+	}
 }
